4638ebd94a16d2c315a9e3f44cc1169447c8b9b6
Bogdan Petre <bogdan.petre@sennder.com>
2023-02-04 19:32:52 +0100

    feat: git signs plugin

--- nvim/lua/bogdan/packer.lua
+++ nvim/lua/bogdan/packer.lua
@@ -94,6 +94,9 @@ return packer.startup(function(use)
 	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
 	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
+	-- git integration
+	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
+
 	-- Variable highlighting
 	use("RRethy/vim-illuminate")
 
