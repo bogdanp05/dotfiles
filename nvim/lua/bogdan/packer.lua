1eb5112e743f2992190982f532b7aee2d6a2ee9b
Bogdan Petre <bogdan.petre0510@gmail.com>
2023-05-30 15:13:16 +0200

    fix: add snippet engine to prevent cmp from crashing from some (rust) lsp suggestions

--- nvim/lua/bogdan/packer.lua
+++ nvim/lua/bogdan/packer.lua
@@ -12,7 +12,7 @@ end
 local packer_bootstrap = ensure_packer() -- true if packer was just installed
 
 -- Autocommand that reloads Neovim and installs/updates/removes plugins when the file is saved
-vim.cmd([[ 
+vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost packer.lua source <afile> | PackerSync
@@ -63,6 +63,9 @@ return packer.startup(function(use)
 	use("hrsh7th/nvim-cmp") -- completion plugin
 	use("hrsh7th/cmp-buffer") -- source for text in buffer
 	use("hrsh7th/cmp-path") -- source for file system paths
+	-- Snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
 
 	-- Managing and installing LSP servers, linters, and formatters
 	use("williamboman/mason.nvim") -- in charge of managing LSP servers, linters & formatters
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -3,6 +3,11 @@ if not cmp_status then
 	return
 end
 
+local luasnip_status, luasnip = pcall(require, "luasnip")
+if not luasnip_status then
+	return
+end
+
 local lspkind_status, lspkind = pcall(require, "lspkind")
 if not lspkind_status then
 	return
@@ -14,6 +19,12 @@ vim.opt.completeopt = "menu,menuone,noselect"
 vim.g.cmptoggle = true
 
 cmp.setup({
+	-- Load snippet support
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 		["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
@@ -34,6 +45,8 @@ cmp.setup({
 		["<S-Tab>"] = function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
 			else
 				fallback()
 			end
@@ -42,6 +55,7 @@ cmp.setup({
 	-- sources for autocompletion
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" }, -- lsp
+		{ name = "luasnip" }, -- snippets
 		{ name = "buffer" }, -- buffer
 		{ name = "path" }, -- file system paths
 	}),
