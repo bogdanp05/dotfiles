a499f343de9232d49f2e1c85df185a2625a8bbb1
Bogdan Petre <bogdan.petre@aiven.io>
2023-07-19 14:27:47 +0200

    fix: configure snippet engine for nvim-cmp

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -3,6 +3,11 @@ if not cmp_status then
 	return
 end
 
+local luasnip_status, luasnip = pcall(require, "luasnip")
+if not luasnip_status then
+	return
+end
+
 local lspkind_status, lspkind = pcall(require, "lspkind")
 if not lspkind_status then
 	return
@@ -11,6 +16,11 @@ end
 vim.opt.completeopt = "menu,menuone,noselect"
 
 cmp.setup({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 		["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
@@ -41,6 +51,7 @@ cmp.setup({
 		{ name = "nvim_lsp" }, -- lsp
 		{ name = "buffer" }, -- buffer
 		{ name = "path" }, -- file system paths
+		-- { name = "luasnip" }, -- snippets
 	}),
 
 	-- configure lspkind for VSCode-like icons
