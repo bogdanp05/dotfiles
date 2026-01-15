2ec85ed6d0d1e99405d63a78ad9c32390c048c93
Bogdan Petre <bogdan.petre@aiven.io>
2026-01-15 15:06:52 +0100

    nvim: rm telescope branch pin

--- nvim/lua/bogdan/packer.lua
+++ nvim/lua/bogdan/packer.lua
@@ -55,7 +55,6 @@ return packer.startup(function(use)
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
 	use({
 		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
 		requires = { { "nvim-telescope/telescope-live-grep-args.nvim" } },
 		config = function()
 			require("telescope").load_extension("live_grep_args") -- fuzzy finder
